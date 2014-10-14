498293cd8d7fbe9cc2b60e025375258fa6fb75dd
Brent Burgoyne <bburgoyne@instructure.com>
2014-10-14 16:45:44 -0600

    Add json type

---
+++ test/types/json.spec.js
@@ -0,0 +1,55 @@
+var should = require('should')
+var sinon = require('sinon')
+var proxyquire = require('proxyquire').noPreserveCache()
+var stubs = {}
+var json = proxyquire('../../lib/types/json', {'./string': stubs})
+
+stubs.to = function (val) { return val }
+stubs.from = function (val) { return val }
+
+describe('The json attribute type', function () {
+
+  beforeEach(function () {
+    sinon.stub(JSON, 'stringify')
+    sinon.stub(JSON, 'parse')
+  })
+
+  afterEach(function () {
+    JSON.stringify.restore()
+    JSON.parse.restore()
+  })
+
+  describe('`to()` method', function () {
+
+    it('should call JSON.stringify with the value', function () {
+      var val = 'foo'
+      json.to(val)
+      sinon.assert.calledWithExactly(JSON.stringify, val)
+    })
+
+    it('shour return the result of JSON.stringify', function () {
+      var expected = 'bar'
+      JSON.stringify.returns(expected)
+      json.to('foo').should.equal(expected)
+    })
+
+  })
+
+  describe('`from()` method', function () {
+
+    it('should call JSON.parse with the value', function () {
+      var val = 'foo'
+      json.from(val)
+      sinon.assert.calledWithExactly(JSON.parse, val)
+    })
+
+    it('shour return the result of JSON.parse', function () {
+      var expected = 'bar'
+      JSON.parse.returns(expected)
+      json.from('foo').should.equal(expected)
+    })
+ 
+  })
+
+})
+
